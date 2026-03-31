93b873a1304df86b774022421eb23a6b8603d655
Galberton <gustavo.borghezan030706@gmail.com>
2026-03-31 17:26:16 -0300

    refac 3

--- internal/handlers/send_message.go
+++ internal/handlers/send_message.go
@@ -60,28 +60,3 @@ func SendMessage(
 	}
 
 }
-
-/*func SendMessage(mediaService *services.MediaService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		var req SendMessageRequest
-
-		err := json.NewDecoder(r.Body).Decode(&req)
-		if err != nil {
-			http.Error(w, "invalid json", http.StatusBadRequest)
-			return
-		}
-
-		err = mediaService.SendTextMessage(req.To, req.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Write([]byte("mensagem enviada"))
-	}
-}*/
