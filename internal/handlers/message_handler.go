30e173c1547e0698889e38597c73652ae4bbaf9f
Galberton <gustavo.borghezan030706@gmail.com>
2026-03-31 17:25:24 -0300

    refac 2

--- internal/handlers/message_handler.go
+++ internal/handlers/message_handler.go
@@ -67,24 +67,3 @@ func MarkAsRead(messageService *services.MessageService) http.HandlerFunc {
 		w.Write([]byte("ok"))
 	}
 }
-
-/*func GetMessages(messageService *services.MessageService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		if r.Method != http.MethodGet {
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		conversationID := r.URL.Query().Get("conversation_id")
-		if conversationID == "" {
-			http.Error(w, "conversation_id required", http.StatusBadRequest)
-			return
-		}
-
-		messages := messageService.GetByConversation(conversationID)
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(messages)
-	}
-}*/
