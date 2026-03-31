5e8e59d10cad7e3139c60ee9bc1586047d72abe8
Galberton <gustavo.borghezan030706@gmail.com>
2026-03-31 17:35:23 -0300

    começou a logar o back com o front

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,21 @@ import (
 	"ZAPS/internal/webhook"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	mediaService := services.NewMediaService()
