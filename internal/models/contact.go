52ff43e2d85b49891928eda5cd51e0a58e406493
Galberton <gustavo.borghezan030706@gmail.com>
2026-05-04 13:49:24 -0300

    Update contact.go

--- internal/models/contact.go
+++ internal/models/contact.go
@@ -1,9 +1,6 @@
 package models
 
-//contact serve para salvar o contato
 type Contact struct {
 	Phone string
 	Name  string
 }
-
-// aqui o pelo funciona como se fosse um molde, depois é usado em outros processos ai fala como +/- vai funcionar
